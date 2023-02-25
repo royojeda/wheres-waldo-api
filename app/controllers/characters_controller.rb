fa33f2fb57dfba37364d38c2249d3b3173df9aa8
Dennis Ojeda <royojeda1@gmail.com>
2023-02-25 23:38:19 +0800

    feat: define Character strong parameters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,3 +1,16 @@
 class CharactersController < ApplicationController
-  def index; end
+  def index
+    @character = Character.locate(character_params)
+
+    render json: @character, only: %i[name x_coordinate y_coordinate]
+  end
+
+  private
+
+  def character_params
+    params.require(:name)
+    params.require(:x_coordinate)
+    params.require(:y_coordinate)
+    params.permit(:name, :x_coordinate, :y_coordinate).to_h.symbolize_keys
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,13 @@
 class Character < ApplicationRecord
+  def self.locate(character_params)
+    character_params => {name:, x_coordinate:, y_coordinate:}
+    Rails.logger.debug "\n\n"
+    Rails.logger.debug name
+    Rails.logger.debug x_coordinate
+    Rails.logger.debug y_coordinate
+    Rails.logger.debug "\n\n"
+
+    # Return all characters for now
+    all
+  end
 end
