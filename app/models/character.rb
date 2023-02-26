ccc949482af8b61f9d59e34c633b0202e03f6ca8
Dennis Ojeda <royojeda1@gmail.com>
2023-02-26 16:51:56 +0800

    feat: return found characters
    The allowed ranges for x and y coordinates for each character is
    hardcoded. The appearance of the targeting box in the front-end
    represents these ranges.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,13 +1,22 @@
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
 class Character < ApplicationRecord
   def self.locate(character_params)
     character_params => {name:, x_coordinate:, y_coordinate:}
-    Rails.logger.debug "\n\n"
-    Rails.logger.debug name
-    Rails.logger.debug x_coordinate
-    Rails.logger.debug y_coordinate
-    Rails.logger.debug "\n\n"
 
-    # Return all characters for now
-    all
+    left_edge = (x_coordinate.to_f - (1.0 / 60))
+    right_edge = x_coordinate.to_f + (1.0 / 60)
+    top_edge = y_coordinate.to_f - (100.0 / 39333)
+    bottom_edge = y_coordinate.to_f + (100.0 / 39333)
+
+    Rails.logger.debug
+    Rails.logger.debug 1.0 / 60
+    Rails.logger.debug 100.0 / 39333
+    Rails.logger.debug
+
+    where(name:,
+          x_coordinate: left_edge..right_edge,
+          y_coordinate: top_edge..bottom_edge)
   end
 end
+
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength
