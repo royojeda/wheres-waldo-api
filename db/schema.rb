850c27ff16cb4cf3b2e796b6e0bbfc4be3374048
Dennis Ojeda <royojeda1@gmail.com>
2023-03-02 12:01:22 +0800

    feat: add Game resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_110739) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_034946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_110739) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "games", force: :cascade do |t|
+    t.string "player_name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
